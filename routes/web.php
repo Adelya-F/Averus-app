4fef9dca3935e446ea5e659ac7c7c049f6e07dc4
fazansaa09-glitch <fazansaa09@gmail.com>
2026-02-23 11:10:35 +0700

    login dasar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin', function () {
+    return "Ini Dashboard Admin";
+})->middleware(['auth']);
+
+Route::get('/guru', function () {
+    return "Ini Dashboard Guru";
+})->middleware(['auth']);
+
+Route::get('/siswa', function () {
+    return "Ini Dashboard Siswa";
+})->middleware(['auth']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
