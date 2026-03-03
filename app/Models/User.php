a9b8476e0ff6a6cad559281ba21da757c90fd602
Putri Jasmine Azzahra Ramadhani <putrijasmine180908@gmail.com>
2026-03-03 07:46:48 +0700

    update fitur register dan user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'school',
+        'class',
+        'hobby',
+        'address',
+        'phone',
+        'parent_name',
+        'parent_phone',
+        'favorite_subject',
+        'instagram',
+        'tiktok',
+        'role',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -34,9 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Attribute casting.
      */
     protected function casts(): array
     {
