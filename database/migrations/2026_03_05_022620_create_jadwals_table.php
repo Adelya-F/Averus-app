f1c4732f23e859cf64563ba4069a16e430c15ef5
Putri Jasmine Azzahra Ramadhani <putrijasmine180908@gmail.com>
2026-04-28 09:48:05 +0700

    jadwal

--- database/migrations/2026_03_05_022620_create_jadwals_table.php
+++ database/migrations/2026_03_05_022620_create_jadwals_table.php
@@ -6,17 +6,34 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
-    {
-        Schema::create('mapels', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_mapel');
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('jadwals', function (Blueprint $table) {
+        $table->id();
+        // ID Guru (yang ngajar)
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+        
+        // ID Kelas (Biar sekali input, semua siswa di kelas itu dapet jadwalnya)
+        $table->foreignId('kelas_id')->constrained('kelas')->onDelete('cascade'); 
+        
+        // ID Mapel
+        $table->foreignId('mapel_id')->constrained('mapels')->onDelete('cascade'); 
+        
+        $table->string('hari'); 
+        $table->time('jam_mulai');
+        $table->time('jam_selesai');
+        $table->timestamps();
+    });
 
+}
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('mapels');
+        Schema::dropIfExists('jadwals');
     }
 };
